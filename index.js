f76befd87d5e2098794f00b1c9652e50b4100e8b
Max Aasbø <max@dira.no>
2017-12-03 22:47:44 +0100

    Implement Google OAuth sign-in using Passport.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
+require('./services/passport.js');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+require('./routes/authRoutes')(app);
 
 // Heroku will set the environment variable with the appropriate port
 const PORT = process.env.PORT || 5000;
