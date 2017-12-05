0ed048e6432e4d365c768d330b0ddca01415130d
Max Aasbø <max@dira.no>
2017-12-05 11:58:25 +0100

    Add styling to navbar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,12 +3,17 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import Header from './Header';
 
+const Landing = () => {
+  return <h2>Landing</h2>;
+};
+
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <div>
+        <div className="container">
           <Header />
+          <Route exact path="/" component={Landing} />
         </div>
       </BrowserRouter>
     </div>
