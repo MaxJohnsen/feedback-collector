7e7bb48318815222a4084c56f9b277bf9e522c9d
Max Aasbø <max@dira.no>
2017-12-06 16:19:23 +0100

    Configure project for production deployment
    Configure back-end to serve front-end on unrecognized routes.
    
    Add new npm script that will be automatically run by Heroku after
    building back-end. The script will:
    1. Install client-side dependencies
    2. Build front-end
    3. Delete client-side dependencies
    -

--- index.js
+++ index.js
@@ -24,6 +24,22 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // To reach this code, the request's route is not recognized,
+  // and we are in a production environment
+
+  // Check if the request are for production assets such as main.js and main.css
+  app.use(express.static('client/build'));
+
+  // If not:
+  // Express will serve up the index.html file (front-end), and React-Router
+  // will use the route to decide which component to show
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve('__dirname', 'client', 'build', 'index.html'));
+  });
+}
+
 // Heroku will set the environment variable with the appropriate port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
